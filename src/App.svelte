90c9641586266050752e42c2c4e1a6b9ebf305cd
Laddge <dev.laddge@gmail.com>
2026-05-07 14:20:02 +0900

    add discord bot

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,32 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
+  import { Discord } from './lib/discord'
+
+  let voiceStates = $state([])
+
+  onMount(() => {
+    const hash = window.location.hash.substring(1).split(',')
+    const d = new Discord(hash[0])
+    d.addEventListener('VOICE_STATE_UPDATE', ev => {
+      const { detail } = ev as CustomEvent
+      if (detail.channel_id == hash[1]) {
+        voiceStates = voiceStates.filter(v => v.user_id != detail.user_id)
+        voiceStates.push(detail)
+      } else {
+        voiceStates = voiceStates.filter(v => v.user_id != detail.user_id)
+      }
+    })
+    d.addEventListener('GUILD_CREATE', ev =>  {
+      const { detail } = ev as CustomEvent
+      voiceStates = detail.voice_states.filter(v => v.channel_id == hash[1]).map(v => ({
+        ...v,
+        member: detail.members.filter(m => m.user.id == v.user_id)[0],
+      }))
+    })
+    d.start()
+  })
 </script>
 
-<div>hello</div>
+{#each voiceStates as v}
+<div>{v.member.user.username} {v.deaf?'sD':''}{v.mute?'sM':''}{v.self_deaf?'D':''}{v.self_mute?'M':''}{v.self_video?'V':''}{v.self_stream?'S':''}</div>
+{/each}
---
+++ src/lib/discord.ts
@@ -0,0 +1,60 @@
+export class Discord extends EventTarget {
+  ws: WebSocket|null = null
+  #token: string
+  #timer: number|null = null
+  #ackHeartbeat: boolean = true
+  #seq: number|null = null
+
+  constructor(token: string) {
+    super()
+    this.#token = token
+  }
+
+  start() {
+    this.ws = new WebSocket('wss://gateway.discord.gg/?v=10&encoding=json')
+    this.ws.onmessage = (ev: MessageEvent) => {
+      const data = JSON.parse(ev.data)
+      this.#seq = data.s
+      // Hello
+      if (data.op == 10) {
+        this.#timer = setInterval(() => {
+          if (!this.#ackHeartbeat) {
+            this.#ackHeartbeat = true
+            this.#timer && clearInterval(this.#timer)
+            this.ws?.close()
+            return
+          }
+          this.#ackHeartbeat = false
+          // Heartbeat
+          this.ws?.send(JSON.stringify({ op: 1, d: this.#seq }))
+        }, data.d.heartbeat_interval)
+      }
+      // Heartbeat ACK
+      if (data.op == 11) {
+        this.#ackHeartbeat = true
+      }
+      // Dispatch
+      if (data.op == 0) {
+        this.dispatchEvent(new CustomEvent(data.t, { detail: data.d }))
+      }
+    }
+    this.ws.onopen = (_ev: Event) => {
+      // Identify
+      this.ws?.send(JSON.stringify({
+        op: 2,
+        d: {
+          token: this.#token,
+          intents: 1<<7 | 1<<0,
+          properties: {
+            os: 'linux',
+            browser: 'vcwatcher',
+            device: 'vcwatcher',
+          },
+        },
+      }))
+    }
+    this.ws.onclose = () => {
+      this.start()
+    }
+  }
+}
