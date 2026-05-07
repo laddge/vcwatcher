bab6752c44abad32e0f6f57c89c55d9fd6a4e701
Laddge <dev.laddge@gmail.com>
2026-05-07 14:33:20 +0900

    add types

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,26 @@
   import { onMount } from 'svelte'
   import { Discord } from './lib/discord'
 
-  let voiceStates = $state([])
+  type Member = {
+    user: {
+      id: string
+      username: string
+    }
+  }
+
+  type VoiceState = {
+    user_id: string
+    channel_id: string
+    deaf: boolean
+    mute: boolean
+    self_deaf: boolean
+    self_mute: boolean
+    self_video: boolean
+    self_stream?: boolean
+    member: Member
+  }
+
+  let voiceStates = $state<VoiceState[]>([])
 
   onMount(() => {
     const hash = window.location.hash.substring(1).split(',')
@@ -18,9 +37,9 @@
     })
     d.addEventListener('GUILD_CREATE', ev =>  {
       const { detail } = ev as CustomEvent
-      voiceStates = detail.voice_states.filter(v => v.channel_id == hash[1]).map(v => ({
+      voiceStates = (detail.voice_states as VoiceState[]).filter(v => v.channel_id == hash[1]).map(v => ({
         ...v,
-        member: detail.members.filter(m => m.user.id == v.user_id)[0],
+        member: (detail.members as Member[]).filter(m => m.user.id == v.user_id)[0],
       }))
     })
     d.start()
